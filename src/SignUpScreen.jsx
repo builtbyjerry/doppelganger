6720d10a78ecf6ec0749d2ea567da00c75c15b5e
Not-Clinton <clintonnjoku27@gmail.com>
2024-07-24 17:41:28 +0100

    Feat:Facescan icon

--- src/SignUpScreen.jsx
+++ src/SignUpScreen.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { ScanFace } from 'lucide-react-native'; 
 
-const SignUpScreen = ({navigation}) => {
+const SignUpScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.box}>
+        <View style={styles.iconContainer}>
+          <ScanFace size={50} color="#000" style={styles.icon} />
+        </View>
         <Text style={styles.text}>Let's scan your face to sign you up</Text>
-        <TouchableOpacity style={styles.button}onPress={() => navigation.navigate('FaceScan')}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('FaceScan')}>
           <Text style={styles.buttonText}>Launch your camera</Text>
         </TouchableOpacity>
       </View>
@@ -28,20 +32,25 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingTop: 20,
+  },
+  iconContainer: {
+    backgroundColor: '#FFFAB8', 
+    marginBottom: 10,
   },
   text: {
-    fontSize: 30,
+    fontSize: 35,
     textAlign: 'center',
-    marginBottom: 50,
+    marginBottom: 30,
   },
   button: {
-    backgroundColor: '#000',
+    backgroundColor: '#FFFAB8', 
     paddingVertical: 15,
     paddingHorizontal: 30,
     borderRadius: 5,
   },
   buttonText: {
-    color: '#FFF',
+    color: '#000',
     fontSize: 16,
     textAlign: 'center',
   },
